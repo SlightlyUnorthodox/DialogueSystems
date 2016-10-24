2367631976e180f47398e449142a058d66fcb491
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-10-24 01:57:48 +0000

    Added demo models to Django project settings. Finalized table relationships. Continuing to define fields and constraints.

--- preliminator/demo/models.py
+++ preliminator/demo/models.py
@@ -2,7 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, RegexValidator
+
+##
+## System user information
+##
 
 # Table for system users
 class User(models.Model):
@@ -13,6 +17,7 @@ class User(models.Model):
 
 	# Generic username requirements
 	username = models.CharField(
+		"Username",
 		max_length = 50, 
 		validators = [MinLengthValidator(8, "Your username must contain at least 8 characters.")],
 		unique = True
@@ -22,11 +27,15 @@ class User(models.Model):
 
 	# Generic password requirements
 	password = models.CharField(
+		"Password",
 		max_length = 50,
 		validators = [MinLengthValidator(8, "Your username must contain at least 8 characters.")]
 		)
 	def __unicode__(self):
 		return self.password
+
+	# Allow many-to-one relation between user and (candidate/recruiter)
+	pass
 
 	
 # Job candidates/applicants information
@@ -37,31 +46,37 @@ class Candidate(models.Model):
 		return self.candidate_id
 	
 	# One-to-one relationship with user
-	user = models.OneToOneField(
+	user = models.ForeignKey(
 		User,
-		on_delete = models.CASCADE,
-		primary_key = TRUE
+		on_delete = models.CASCADE
 	)
 	def __unicode__(self):
 		return self.user
 	
 	# Candidate first name
 	first_name = models.CharField(
+		"First name",
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.first_name
 
 	# Candidate last name
 	last_name = models.CharField(
+		"Last name",
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.last_name
 
+	##
+	## TODO: Fill with remaining fields from candidate form
+	##
 
+	# Allow many-to-one relation between candidate and interview
+	pass
 
 # Job recruiter information
 class Recruiter(models.Model):
@@ -71,10 +86,9 @@ class Recruiter(models.Model):
 		return self.recruiter_id
 	
 	# One-to-one relationship with user
-	user = models.OneToOneField(
+	user = models.ForeignKey(
 		User,
-		on_delete = models.CASCADE,
-		primary_key = TRUE
+		on_delete = models.CASCADE
 	)
 	def __unicode__(self):
 		return self.user
@@ -82,7 +96,7 @@ class Recruiter(models.Model):
 	# Candidate first name
 	first_name = models.CharField(
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.first_name
@@ -90,7 +104,7 @@ class Recruiter(models.Model):
 	# Candidate last name
 	last_name = models.CharField(
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.last_name
@@ -98,7 +112,7 @@ class Recruiter(models.Model):
 	# Recruiter's employer/company
 	employer = models.CharField(
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.employer
@@ -106,24 +120,17 @@ class Recruiter(models.Model):
 	# Recruiter's title
 	title = models.CharField(
 		max_length = 50,
-		validators = RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')
+		validators = [RegexValidator(r'^[a-zA-Z]*$', 'Only alphabetic characters allowed')]
 	)
 	def __unicode__(self):
 		return self.title
 
-# Pre-interview survey
-class PreSurvey(models.Model):
-	# Key field def
-	presurvey_id = models.AutoField(primary_key = True)
-	def __unicode__(self):
-		return self.presurvey_id
+	# Allow many-to-one relation between recruiter and interview
+	pass
 
-# Post-interview survey
-class PostSurvey(models.Model):
-	# Key field def
-	postsurvey_id = models.AutoField(primary_key = True)
-	def __unicode__(self):
-		return self.postsurvey_id
+##
+## Primary log for interview output/decisions
+##
 
 # Interview metadata
 class Interview(models.Model):
@@ -132,12 +139,71 @@ class Interview(models.Model):
 	def __unicode__(self):
 		return self.interview_id
 
+	# Interview candidate
+	candidate = models.ForeignKey(
+		Candidate,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.candidate
+	
+	# Interview recruiter
+	recruiter = models.ForeignKey(
+		Recruiter,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.recruiter
+	
+	
+##
+## Interview sub-tables
+##
+
+# Pre-interview survey
+class PreSurvey(models.Model):
+	# Key field def
+	presurvey_id = models.AutoField(primary_key = True)
+	def __unicode__(self):
+		return self.presurvey_id
+
+	# One-to-one relationship with interview
+	interview = models.ForeignKey(
+		Interview,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.interview
+
+# Post-interview survey
+class PostSurvey(models.Model):
+	# Key field def
+	postsurvey_id = models.AutoField(primary_key = True)
+	def __unicode__(self):
+		return self.postsurvey_id
+
+	# One-to-one relationship with interview
+	interview = models.ForeignKey(
+		Interview,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.interview
+
 # Interview/screening transcripts
 class Transcript(models.Model):
 	# Key field def
 	transcript_id = models.AutoField(primary_key = True)
 	def __unicode__(self):
 		return self.transcript_id
+
+	# One-to-one relationship with interview
+	interview = models.ForeignKey(
+		Interview,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.interview	
 
 
 # Interview feedback fields
@@ -146,3 +212,11 @@ class Feedback(models.Model):
 	feedback_id = models.AutoField(primary_key = True)
 	def __unicode__(self):
 		return self.feedback_id
+
+	# One-to-one relationship with interview
+	interview = models.ForeignKey(
+		Interview,
+		on_delete = models.CASCADE
+	)
+	def __unicode__(self):
+		return self.interview	
