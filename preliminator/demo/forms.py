88bdde9d7e3311256e98dfb457e0cfd684a93dfd
Sara Lichtenstein <saralich@ufl.edu>
2016-10-25 20:18:03 -0400

    adjustments to forms.py

--- preliminator/demo/forms.py
+++ preliminator/demo/forms.py
@@ -8,11 +8,40 @@ class SignInForm(forms.Form):
 	password = forms.CharField(label = 'Password', min_length = 8, widget = forms.PasswordInput(), required = True)
 
 #Candidate information, specific
-class CandidateInformation(forms.Form):
+class CandidateForm(forms.Form):
 	firstName = forms.CharField(label = 'FirstName', min_length = 1, max_length = 100, required = True)
 	lastName = forms.CharField(label = 'LastName', min_length = 1, max_length = 100, required = True)
-	highestEducation = forms.CharField(label = 'HighestEducation', min_length = 1, max_length = 1, required = True)
-	educationStatus = forms.CharField(label = 'EducationStatus', min_length = 1, max_length = 1, required = True)
+	highestEducation = forms.CharField(label = 'HighestEducation', max_length = 1, required = True)
+	educationStatus = forms.CharField(label = 'EducationStatus', max_length = 1, required = True)
 	programField = forms.CharField(label = 'MajorField', min_length = 1, max_length = 50, required = True)
 	yearsExperience = forms.IntegerField(label = 'YearsExperience', min_value = 1, max_value = 20, required = True)
 	relevantEmployer = forms.CharField(label = 'RelevantEmployer', min_length = 1, max_length = 50, required = True)
+
+#Recruiter information
+class RecruiterForm(forms.Form):
+	firstName = forms.CharField(label = 'FirstName', min_length = 1, max_length = 100, required = True)
+	lastName = forms.CharField(label = 'LastName', min_length = 1, max_length = 100, required = True)
+	employer = forms.CharField(label = 'Employer', min_length = 1, max_length = 50, required = True)
+	title = forms.CharField(label = 'Title', min_length = 1, max_length = 50, required = True)
+
+#PreSurvey
+class PreSurveyForm(forms.Form):
+	questionOneResponse = forms.CharField(label = 'QuestionOneResponse', max_length = 1, required = True)
+	questionTwoResponse = forms.CharField(label = 'QuestionTwoResponse', max_length = 1, required = True)
+	questionThreeResponse = forms.CharField(label = 'QuestionThreeResponse', max_length = 1, required = True)
+	questionFourResponse = forms.CharField(label = 'QuestionFourResponse', max_length = 1, required = True)
+	questionFiveResponse = forms.CharField(label = 'QuestionFiveResponse', max_length = 1, required = True)
+
+#PostSurvey
+class PostSurveyForm(forms.Form):
+	questionOneResponse = forms.CharField(label = 'QuestionOneResponse', max_length = 1, required = True)
+	questionTwoResponse = forms.CharField(label = 'QuestionTwoResponse', max_length = 1, required = True)
+	questionThreeResponse = forms.CharField(label = 'QuestionThreeResponse', max_length = 1, required = True)
+	questionFourResponse = forms.CharField(label = 'QuestionFourResponse', max_length = 1, required = True)
+	questionFiveResponse = forms.CharField(label = 'QuestionFiveResponse', max_length = 1, required = True)
+
+#Transcript
+class TranscriptForm(forms.Form):
+	speakerChoice = forms.CharField(label = 'Speaker', max_length = 1, required = True)
+	lineNumber = forms.IntegerField(label = 'LineNumber', min_value = 1, max_value = 2000)
+	lineContents = forms.CharField(label = 'LineContent', min_length = 1, max_length = 2000)
