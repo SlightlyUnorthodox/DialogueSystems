6e8236f552eb10e694ae48f8921dcd23ce2621ea
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-10-28 12:47:23 +0000

    modified forms solution for choice-based responses. Removed .html files in /css

--- preliminator/demo/forms.py
+++ preliminator/demo/forms.py
@@ -9,39 +9,79 @@ class SignInForm(forms.Form):
 
 #Candidate information, specific
 class CandidateForm(forms.Form):
-	firstName = forms.CharField(label = 'FirstName', min_length = 1, max_length = 100, required = True)
-	lastName = forms.CharField(label = 'LastName', min_length = 1, max_length = 100, required = True)
-	highestEducation = forms.CharField(label = 'HighestEducation', max_length = 1, required = True)
-	educationStatus = forms.CharField(label = 'EducationStatus', max_length = 1, required = True)
-	programField = forms.CharField(label = 'MajorField', min_length = 1, max_length = 50, required = True)
-	yearsExperience = forms.IntegerField(label = 'YearsExperience', min_value = 1, max_value = 20, required = True)
-	relevantEmployer = forms.CharField(label = 'RelevantEmployer', min_length = 1, max_length = 50, required = True)
+	firstName = forms.CharField(label = 'First Name', min_length = 1, max_length = 100, required = True)
+	lastName = forms.CharField(label = 'Last Name', min_length = 1, max_length = 100, required = True)
+	
+	# Education levels
+	EDU_LEVELS = (
+		('N', 'None'),
+		('H', 'High School Diploma'),
+		('T', 'Technical Diploma'),
+		('A', 'Associate\'s Degree'),
+		('B', 'Bachelor\'s Degree'),
+		('M', 'Master\'s Degree'),
+		('D', 'Doctorate Degree')
+	)
+
+	highestEducation = forms.ChoiceField(choices = EDU_LEVELS, label = 'Highest Education Level', required = True)
+	
+	# Education status levels
+	EDU_STATUS = (
+		('G', 'Graduated'),
+		('I', 'In Progress'),
+		('N', 'Not Complete')
+	)
+
+	educationStatus = forms.ChoiceField(choics = EDU_STATUS, label = 'Education Status', required = True)
+	programField = forms.CharField(label = 'Major/Field', min_length = 1, max_length = 50, required = True)
+	yearsExperience = forms.IntegerField(label = 'Years Experience', min_value = 1, max_value = 20, required = True)
+	relevantEmployer = forms.CharField(label = 'Relevant Employer', min_length = 1, max_length = 50, required = True)
 
 #Recruiter information
 class RecruiterForm(forms.Form):
-	firstName = forms.CharField(label = 'FirstName', min_length = 1, max_length = 100, required = True)
-	lastName = forms.CharField(label = 'LastName', min_length = 1, max_length = 100, required = True)
+	firstName = forms.CharField(label = 'First Name', min_length = 1, max_length = 100, required = True)
+	lastName = forms.CharField(label = 'Last Name', min_length = 1, max_length = 100, required = True)
 	employer = forms.CharField(label = 'Employer', min_length = 1, max_length = 50, required = True)
 	title = forms.CharField(label = 'Title', min_length = 1, max_length = 50, required = True)
 
 #PreSurvey
 class PreSurveyForm(forms.Form):
-	questionOneResponse = forms.CharField(label = 'QuestionOneResponse', max_length = 1, required = True)
-	questionTwoResponse = forms.CharField(label = 'QuestionTwoResponse', max_length = 1, required = True)
-	questionThreeResponse = forms.CharField(label = 'QuestionThreeResponse', max_length = 1, required = True)
-	questionFourResponse = forms.CharField(label = 'QuestionFourResponse', max_length = 1, required = True)
-	questionFiveResponse = forms.CharField(label = 'QuestionFiveResponse', max_length = 1, required = True)
+
+	# Predefine possible response in pre-survey
+	RESPONSES = (
+		(1, 'Strongly disagree'),
+		(2, 'Disagree'),
+		(3, 'Neutral'),
+		(4, 'Agree'),
+		(5, 'Strongly Agree')
+	)
+
+	questionOneResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question One Response', required = True)
+	questionTwoResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Two Response', required = True)
+	questionThreeResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Three Response', required = True)
+	questionFourResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Four Response', required = True)
+	questionFiveResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Five Response', required = True)
 
 #PostSurvey
 class PostSurveyForm(forms.Form):
-	questionOneResponse = forms.CharField(label = 'QuestionOneResponse', max_length = 1, required = True)
-	questionTwoResponse = forms.CharField(label = 'QuestionTwoResponse', max_length = 1, required = True)
-	questionThreeResponse = forms.CharField(label = 'QuestionThreeResponse', max_length = 1, required = True)
-	questionFourResponse = forms.CharField(label = 'QuestionFourResponse', max_length = 1, required = True)
-	questionFiveResponse = forms.CharField(label = 'QuestionFiveResponse', max_length = 1, required = True)
+
+	# Predefine possible response in pre-survey
+	RESPONSES = (
+		(1, 'Strongly disagree'),
+		(2, 'Disagree'),
+		(3, 'Neutral'),
+		(4, 'Agree'),
+		(5, 'Strongly Agree')
+	)
+
+	questionOneResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question One Response', required = True)
+	questionTwoResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Two Response', required = True)
+	questionThreeResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Three Response',required = True)
+	questionFourResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Four Response', required = True)
+	questionFiveResponse = forms.ChoiceField(choices = RESPONSES, label = 'Question Five Response', required = True)
 
 #Transcript
 class TranscriptForm(forms.Form):
 	speakerChoice = forms.CharField(label = 'Speaker', max_length = 1, required = True)
-	lineNumber = forms.IntegerField(label = 'LineNumber', min_value = 1, max_value = 2000)
-	lineContents = forms.CharField(label = 'LineContent', min_length = 1, max_length = 2000)
+	lineNumber = forms.IntegerField(label = 'Line Number', min_value = 1, max_value = 2000)
+	lineContents = forms.CharField(label = 'Line Content', min_length = 1, max_length = 2000)
