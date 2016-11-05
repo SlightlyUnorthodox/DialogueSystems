9bfbf0c385070c3ec73eec8e9328acabc5538616
Pocathoughts <christinemoore321@gmail.com>
2016-11-04 21:19:40 -0400

    savage

--- preliminator/template/js/addChatText.js
+++ preliminator/template/js/addChatText.js
@@ -1,13 +1,43 @@
-function create(htmlStr) {
+// method to dynamically add javascript into the interview page chat box
+// user will be 0 for interviewer and 1 for candidate
+function create(htmlStr, textToAdd, user) {
     var frag = document.createDocumentFragment(),
         temp = document.createElement('div');
+    
+    // automatically changes the html to add
     temp.innerHTML = htmlStr;
+    
     while (temp.firstChild) {
         frag.appendChild(temp.firstChild);
       }
         return frag;
     }
 
-    var fragment = create('<div class="someclass"><a href="www.example.com"><p>some text</p></a></div>'); 
+    // the intro to each message added into the chat conversation
+    var introString = '<li class="media"><div class="media-body"><div class="media">';
 
-    document.body.insertBefore(fragment, document.body.childNodes[0]);
+    // the header for the next part of the html chunk is dependent on interviewer or candidate
+    var interviewer = '<a class="pull-left" href="#"><img class="media-object img-circle " src="../bootstrap-chat-example/assets/img/interviewer.png" /></a><div class="media-body" >';
+    var candidate = '<a class="pull-left" href="#"><img class="media-object img-circle " src="../bootstrap-chat-example/assets/img/cat.png" /></a><div class="media-body" >';
+    
+    // closing tags
+    var endingString = '</div></div></div></li>';
+
+    // yay javascript just uses + signs to add things together
+    if(user == 0){
+        var htmlToAdd = introString + interviewer + textToAdd + endingString;
+
+    }
+    else if (user == 1){
+        var htmlToAdd = introString + candidate + textToAdd + endingString;
+    }
+    else{
+        console.log('yo you fucked up')
+    }
+
+
+    document.getElementById("add-messages").appendChild(fragment);     
+    //document.body.insertBefore(fragment, document.body.childNodes[0]);
+
+
+
