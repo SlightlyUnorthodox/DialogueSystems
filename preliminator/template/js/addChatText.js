436d92a31bf48e84510975de14b6c9a1a8f85819
Pocathoughts <christinemoore321@gmail.com>
2016-11-05 20:48:06 -0400

    adds in text to webchate, super trivial needs refining, but yay

--- preliminator/template/js/addChatText.js
+++ preliminator/template/js/addChatText.js
@@ -4,9 +4,61 @@
 // build fragment and then add it to the document of interest
 //var myLink = document.getElementById('clickMe');
 
-function populate(textToPrint){
-  alert(textToPrint);
+var inputString;
+
+function pop(user){
+    // create fragment to add in
+    var frag = document.createDocumentFragment();
+
+    var contents = frag.appendChild(document.createElement('blockquote'));
+
+    contents = contents.appendChild(document.createElement('p'));
+    contents.appendChild(document.createTextNode('Always two there are'));
+
+
+    //pulls the correct input tag
+    var inputTag = document.getElementById('banana');
+    
+    // grabs the entered value string
+    inputString = inputTag.value;
+    
+    // check to make sure its still linked
+    if(user == 1){
+        alert(inputString);
+    }
+    
+    // get the right div tag
+    var divNeeded = document.getElementById("add-messages");
+
+    divNeeded.appendChild(frag);
+
+    // the intro to each message added into the chat conversation
+    var introString = '<li class="media"><div class="media-body"><div class="media">';
+
+    // the header for the next part of the html chunk is dependent on interviewer or candidate
+    var interviewer = '<a class="pull-left" href="#"><img class="media-object img-circle " src="../bootstrap-chat-example/assets/img/interviewer.png" /></a><div class="media-body" >';
+    var candidate = '<a class="pull-left" href="#"><img class="media-object img-circle " src="../bootstrap-chat-example/assets/img/cat.png" /></a><div class="media-body" >';
+    
+    // closing tags
+    var endingString = '</div></div></div></li>';
+
+    // yay javascript just uses + signs to add things together
+    if(user == 0){
+        var htmlToAdd = introString + interviewer + inputString + endingString;
+    }
+    else if (user == 1){
+        var htmlToAdd = introString + candidate + inputString + endingString;
+    }
+    else{
+        alert('yo you fucked up')
+    }
+
+    divNeeded.appendChild(document.createElement('div'));
 }
+
+// TO DO
+// need to grab the value of the input line above to submit that data on button
+// click
 
 /*
 function populate(htmlStr, textToAdd, user) {
