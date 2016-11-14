b0d82b010f24353da30ab7a1cfdc25c29d3882ab
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-11-14 05:26:53 +0000

    Slowly but surely fixing things that are logically broken

--- preliminator/demo/dialogue.py
+++ preliminator/demo/dialogue.py
@@ -8,15 +8,15 @@ import re
 from .models import Candidate, Interview, User, Recruiter, PreSurvey, PostSurvey, Transcript, Feedback
 
 # TODO: Expand as more patterns are identified
-affirmative_patterns = re.compile("yes|mhm|uhuh|okay")
-negative_patterns = re.compile("no")
+affirmative_patterns = re.compile("(yes)|(mhm)|(uhuh)|(okay)")
+negative_patterns = re.compile("(no)")
 any_pattern = re.compile(".?")
-likert_patterns_one =  re.compile("one|1")
-likert_patterns_two =  re.compile("two|2")
-likert_patterns_three =  re.compile("three|3")
-likert_patterns_four =  re.compile("four|4")
-likert_patterns_five =  re.compile("five|5")
-gpa_patterns = re.compile("([zero|one|two|three|four]\bpoint\b[zero|one|two|three|four|five|six|seven|eight|nine])|([0-4]\.\d{2})")
+likert_patterns_one =  re.compile("(one)|1")
+likert_patterns_two =  re.compile("(two)|2")
+likert_patterns_three =  re.compile("(three)|3")
+likert_patterns_four =  re.compile("(four)|4")
+likert_patterns_five =  re.compile("(five)|5")
+gpa_patterns = re.compile("((zero|one|two|three|four) point( zero| one| two| three| four| five| six| seven| eight| nine){1,2})|([0-4]\.\d{1,2})")
 
 class DialogueManager:
 	def __init__(self, candidate_id = 1, interview_id = 1):
@@ -44,7 +44,7 @@ class DialogueManager:
 		self.current_speaker = 'system'
 		self.proceed = False
 		self.cycle_timeout = 0
-
+		self.validation = False
 		# Store recent utterances
 		self.current_system_utterance = ""
 		self.current_user_utterance = ""
@@ -103,7 +103,158 @@ class DialogueManager:
 				'patterns':{
 					"gpa": gpa_patterns,
 				},
-				'grounding':["You entered your GPA as " + str(self.user_response_value) + ", yes?"],
+				'grounding':["You entered your GPA as: "],
+				'bad_entry':["Please enter GPA as either '#.#' or 'number point number'"]},
+		)
+
+		# Job-driven pairs
+		self.job_acts = (
+			{
+				'utterances': ["On a scale of 1 to 5, 'one' being no experience and five 'expert', what level of experience would you say you have with Git?",
+				"If you had to rate your experience with Git or Github on a scale of 1 (low) to 5 (high), what would you rate it?"],
+				'patterns':{
+				 1: likert_patterns_one,
+				 2: likert_patterns_two,
+				 3: likert_patterns_three,
+				 4: likert_patterns_four,
+				 5: likert_patterns_five,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value from '1' to '5' or 'one' to 'five'."]},
+			{ 
+				'utterances': ["On a scale of 1 to 5, 'one' being no experience and five 'expert', what level of experience would you say you have with Java?",
+				"If you had to rate your experience with Java on a scale of 1 (low) to 5 (high), what would you rate it?"],
+				'patterns':{
+				 1: likert_patterns_one,
+				 2: likert_patterns_two,
+				 3: likert_patterns_three,
+				 4: likert_patterns_four,
+				 5: likert_patterns_five,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value from '1' to '5' or 'one' to 'five'."]},
+		)
+
+
+		# Eligibility pairs
+		self.eligibility_acts = (
+			{ 
+				'utterances':["Are you a United States citizen?", 
+				"Are you eligible for employment in the United States?"],
+				'patterns':{
+				 "yes": affirmative_patterns,
+				 "no": negative_patterns,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+			{ 
+				'utterances':["Will you at any time require visa-sponsorship to continue working?",
+				"Do you require visa-sponsorship to work in the US?"],
+				'patterns':{
+				 "yes": affirmative_patterns,
+				 "no": negative_patterns,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+			{ 
+				'utterances':["Are you a United States citizen?", 
+				"Are you eligible for employment in the United States?"],
+				'patterns':{
+				 "yes": affirmative_patterns,
+				 "no": negative_patterns,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+			{ 
+				'utterances':["Have you ever been convicted of a felony?"],
+				'patterns':{
+				 "yes": affirmative_patterns,
+				 "no": negative_patterns,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+			{ 
+				'utterances':["Do you require any accomodations in order to complete your work?"
+				"Will you need any accomodations to complete the work described?"],
+				'patterns':{
+				 "yes": affirmative_patterns,
+				 "no": negative_patterns,
+				},
+				'grounding':["You entered the value: "],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+		)
+
+		# Closing state pairs
+		self.closing_acts = (
+			{ 
+				'utterances':["Well I believe we are out of time. Thank you for taking the time to try the Preliminator demo and have a good day."],
+				'patterns':{
+				"any": any_pattern,
+				},
+				'grounding':["This is an example grounding sentence."],
+				'bad_entry':["Please enter a value 'yes' or 'no'."]},
+
+		)
+
+		# Dialogue component space information
+		self.state_set = collections.OrderedDict([
+			('greeting', [0, self.greeting_acts]), # 0 - incomplete, 1 - complete
+			('resume', [0, self.resume_acts]),
+			('job', [0, self.job_acts]),
+			('eligibility', [0, self.eligibility_acts]),
+			('closing', [0, self.closing_acts]),
+		])
+
+	## Dialogue Manager Utilities
+
+	def __check_timeout(self):
+		# If difference in start and current time is greater than max
+		#	send signal to cancel process.
+
+		# Renew current time
+		self.current_time = datetime.datetime.now()
+		self.run_time = self.start_time - self.current_time
+
+		# Check dif
+		if(divmod(self.run_time.total_seconds(), 60)[0] > self.max_time):
+			# System has surpassed max time
+			return(1)
+
+		# System still has time
+		return(0)
+
+	def __populate_fields(self):
+		# Try and populate all resume fields
+		for key in self.resume_set:
+			if(self.resume_set[key][0] == 0):
+				self.resume_set[key] = [1, getattr(self.candidate, key)]
+				self.resume_set[key][0] = 1
+
+	def __update_dialogue_acts(self):
+
+		print(self.user_response_value)
+
+		# Greeting state pairs
+		self.greeting_acts =  (
+			{
+				'utterances': ["Hello " + str(self.resume_set['first_name'][1]) + ", my name is Preliminator. We'll be conducting a brief interview today. Ready to begin?",
+				"Hello " + str(self.resume_set['first_name'][1]) + ", I'll be conducting a brief screening interview. Ready to begin?"],
+				'patterns':{
+					"name": any_pattern,
+				},
+				'grounding':["To end the demo at any time, please speak or enter 'quit'. Thank you."],
+				'bad_entry':["No bad entry"]},
+		)
+
+		# Resume-driven pairs
+		self.resume_acts = (
+			{ 
+				'utterances': ["I didn't see you enter your GPA, to the best of your knowledge, what is your current GPA?",
+				"Would you mind sharing your expected GPA at time of graduation?"],
+				'patterns':{
+					"gpa": gpa_patterns,
+				},
+				'grounding':[str("You entered your GPA as " + str(self.user_response_value) + ", yes?")],
 				'bad_entry':["Please enter GPA as either '#.#' or 'number point number'"]},
 		)
 
@@ -195,42 +346,8 @@ class DialogueManager:
 				'bad_entry':["Please enter a value 'yes' or 'no'."]},
 
 		)
-
-		# Dialogue component space information
-		self.state_set = collections.OrderedDict([
-			('greeting', [0, self.greeting_acts]), # 0 - incomplete, 1 - complete
-			('resume', [0, self.resume_acts]),
-			('job', [0, self.job_acts]),
-			('eligibility', [0, self.eligibility_acts]),
-			('closing', [0, self.closing_acts]),
-		])
-
-	## Dialogue Manager Utilities
-
-	def __check_timeout(self):
-		# If difference in start and current time is greater than max
-		#	send signal to cancel process.
-
-		# Renew current time
-		self.current_time = datetime.datetime.now()
-		self.run_time = self.start_time - self.current_time
-
-		# Check dif
-		if(divmod(self.run_time.total_seconds(), 60)[0] > self.max_time):
-			# System has surpassed max time
-			return(1)
-
-		# System still has time
-		return(0)
-
-	def __populate_fields(self):
-		# Try and populate all resume fields
-		for key in self.resume_set:
-			if(self.resume_set[key][0] == 0):
-				self.resume_set[key] = [1, getattr(self.candidate, key)]
-				self.resume_set[key][0] = 1
-
-	def __check_state(self):
+	
+	def check_state(self):
 
 		# Check to see if current state has remaining utterances ## TODO: make more sophisticated
 		if ((self.dialogue_state_act + 1) >= len(self.state_set[self.dialogue_state][1])):
@@ -243,8 +360,6 @@ class DialogueManager:
 
 		# Assign current state as first zero-completion state
 		for key in self.state_set:
-			#print("Key: " + key)
-			#print(self.state_set[key][0])
 
 			if (self.state_set[key][0] == 0):
 				self.dialogue_state = key
@@ -260,6 +375,11 @@ class DialogueManager:
 	# Processes text into dialogue manager
 	def process_speech(self, input):
 		
+		# Check state iteration
+		if self.proceed == True:
+			print("RUNNING: Check State")
+			self.check_state()
+
 		# Do something with input
 		self.current_user_utterance = input.lower()
 
@@ -269,8 +389,9 @@ class DialogueManager:
 			return
 
 		# Prevent death spirals
-		if self.cycle_timeout >= 3:
+		if self.cycle_timeout >= 2:
 			self.dialogue_phrase = 'utterances'
+			self.cycle_timeout = 0
 			self.proceed = True
 			return
 
@@ -279,28 +400,33 @@ class DialogueManager:
 		# 	# Design easter egg here
 		# 	return
 
-		print("Received input: " + self.current_user_utterance + "\n")
-		print("Dialogue Phrase: " + str(self.dialogue_phrase) + "\n")
+		print("Received input: " + self.current_user_utterance )
+		print("Dialogue Phrase: " + str(self.dialogue_phrase))
 		print("Dialogue State: " + str(self.dialogue_state) + "\n")
 
 		# Only run processing on select states
-		if self.dialogue_state in ('job', 'eligibility'): # ('resume', 'job', 'eligibility')
+		if self.dialogue_state in ('resume', 'job', 'eligibility'): # ('resume', 'job', 'eligibility')
 
 			# Check if user input makes sense
-			if self.dialogue_phrase == 'utterances':
+			if self.dialogue_phrase == 'utterances' and self.validation == True:
+
+				# Prepare to not check standard input
+				self.validation = False
 
 				# Check for matching patterns
 				for key in self.state_set[self.dialogue_state][1][self.dialogue_state_act]['patterns']:
-					
+					print("Key: " + str(key) + "\n")
+
 					# Compile regex pattern and check for match
 					pattern = self.state_set[self.dialogue_state][1][self.dialogue_state_act]['patterns'][key]
 					if re.search(pattern, self.current_user_utterance):
-						print("Key: " + str(key))
+						print("Key Matched: " + str(key))
 
 						self.user_response = key
 						# Get matching value
-						self.user_response_value = pattern.findall(self.current_user_utterance)
-
+						match = pattern.findall(self.current_user_utterance)
+						self.user_response_value = str(match[0][0])
+						
 						#self.bad_entry = False
 
 						self.dialogue_phrase ='grounding'
@@ -309,9 +435,18 @@ class DialogueManager:
 						# End process
 						return
 
+
 				self.dialogue_phrase = 'bad_entry'
 				self.proceed = False
 				# End process
+				return
+
+			elif self.dialogue_phrase == 'utterances':
+				# Prepare to check input next round
+				self.validation = True
+				self.proceed = False
+
+				# Return now
 				return
 
 			# Otherwise reconcile grounding or bad state
@@ -320,16 +455,19 @@ class DialogueManager:
 				# Check for affirmative string
 				pattern = affirmative_patterns
 				if re.search(pattern, self.current_user_utterance):
-					self.user_response_value = pattern.findall(self.current_user_utterance)
-					
+					match = pattern.findall(self.current_user_utterance)
+					self.user_response_value = str(match[0][0])
+
 					# Iterate dialogue state
 					self.proceed = True
+					self.cycle_timeout = 0
 					self.utterances = True
 
 					# End process
 					return
 
 				self.dialogue_phrase = 'utterances'
+				self.cycle_timeout += 1
 				self.proceed = False
 
 				# End process
@@ -339,6 +477,7 @@ class DialogueManager:
 
 				# Set back to utterances, don't iterate, and retry
 				self.dialogue_phrase == 'utterances'
+				self.cycle_timeout += 1
 				self.proceed = False
 
 				# End process
@@ -346,6 +485,8 @@ class DialogueManager:
 
 		# Iterate dialogue state
 		self.proceed = True
+		self.dialogue_phrase = 'utterances'
+		self.cycle_timeout = 0
 
 		# End process
 		return
@@ -356,17 +497,23 @@ class DialogueManager:
 	def speak(self):
 
 		# Check timeout
-		self.__check_timeout()
+		#self.__check_timeout()
+
+		print("System: dialogue_state=" + str(self.dialogue_state))
+		print("System: dialogue_state_act=" + str(self.dialogue_state_act))
+		print("System: dialogue_phrase=" + str(self.dialogue_phrase))
+
+		# Update dialogue acts
+		#self.__update_dialogue_acts()
 
 		# Update current system utterance
 		self.current_system_utterance = random.choice(self.state_set[self.dialogue_state][1][self.dialogue_state_act][self.dialogue_phrase]) # [self.dialogue_state_utterance]
 
-		# Log system utterance
-		print("System: " + str(self.current_system_utterance) + "\n")
+		if self.dialogue_phrase == 'grounding':
+			self.current_system_utterance = self.current_system_utterance + str(self.user_response_value) + "?"
 
-		# Check state iteration
-		if self.proceed == True:
-			self.__check_state()
+		# Log system utterance
+		print("System: " + str(self.current_system_utterance))
 
 		# Return system utterance to front-end
 		return(self.current_system_utterance)
