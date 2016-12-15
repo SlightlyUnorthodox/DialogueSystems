744697c017510ece79ba266dee9179ee12eec4cc
SlightlyUnorthodox <gerts.dax1@gmail.com>
2016-12-15 03:54:03 +0000

    Usable version of db.sqlite, converted json files, initial sys_evaluation work

--- preliminator/evaluation/sys_evaluation.R
+++ preliminator/evaluation/sys_evaluation.R
@@ -9,6 +9,9 @@ for(file in dir()[grep(".json", dir())]) {
   # Strip file tag
   file.name <- gsub(".json", "", file)
   
+  # Catch empty 'feedback.json' file exception
+  if(file.name == 'feedback') next
+  
   # Read in data from json file
   assign(file.name, fromJSON(paste(readLines(file, warn = FALSE), collapse = "")))
   
@@ -30,13 +33,32 @@ for(file in dir()[grep(".json", dir())]) {
 # Manually fix interview number bug (always '1')
 curr_interview <- 1
 for(row in 2:nrow(transcript)) {
-  if (transcript[row, "line_number"] == 0) {
+
+  if (any(grepl("Hello", transcript[row, "line_contents"])) == TRUE) {
     curr_interview <- curr_interview + 1
   }
+  
   transcript[row, "interview"] <- curr_interview
+  
 }
 
+# Manually identify subset of usable data (starting at row 227)
+candidate <- candidate[227:nrow(candidate),]
 
+# Identify length of usable chunk
+tail.len <- nrow(candidate)
+
+# Extract relevant subset of transcript (last 21 sets of each)
+transcript$interview <- as.numeric(transcript$interview)
+transcript.limit <- as.numeric(tail(transcript$interview,1)) - tail.len
+transcript <- transcript[which(transcript$interview >= transcript.limit),]
+
+# Extract relevant susbset of pre-/post-surveys
+presurvey <- presurvey[(nrow(presurvey) - tail.len + 1):nrow(presurvey),]
+postsurvey <- postsurvey[(nrow(postsurvey) - tail.len + 1):nrow(postsurvey),]
+
+# Set global statistics
+sample.size <- nrow(candidate)
 
 
 # Find best predictor of user satisfaction
